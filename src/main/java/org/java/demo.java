2b97cd18633214ea9981f1bb8de57a024540f8e1
1992849144 <1992849144@qq.com>
2020-06-03 08:07:52 +0800

    1111

--- src/main/java/org/java/demo.java
+++ src/main/java/org/java/demo.java
@@ -5,6 +5,7 @@ public class demo {
     public static void main(String[] args) {
         Integer  a=20;
         Integer b=30;
+        Integer c=40;
         System.out.println(a);
         System.out.println(b);
     }
